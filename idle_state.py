4653f86a8a213a9199ed10e68c067f904cf78b17
Sourav <souravunnikrishnan123@gmail.com>
2025-11-29 18:35:48 +0000

    deletes

--- idle_state.py
+++
@@ -1,7 +0,0 @@
-from Frame_data import BaseState, FrameContext
-
-class idle_state(BaseState):
-    def __init__(self):
-        super().__init__("idle")
-    def do_action(self, ctx: FrameContext):
-        return "searching_door_plane_state"
--- sort_lines_by_y.py
+++
@@ -1,3 +0,0 @@
-def sort_lines_by_y(line_points):
-    # Sort points by y-coordinate (ascending)
-    return sorted(line_points, key=lambda pt: pt[1])
